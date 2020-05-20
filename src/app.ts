6fa7b111e4f64efa01f1274110a5ce5451ca8371
paritosh9199 <paritoshsrivastava9199@gmail.com>
2020-05-20 21:28:08 +0530

    Minor fix

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ let whitelist = require("../whitelist.json")
 var corsOptions: cors.CorsOptions = {
     //@ts-ignore
     origin: function (origin: string, callback: any) {
-        // console.log(whitelist.indexOf(origin), origin)
+        console.log(whitelist.indexOf(origin), origin)
         if (process.env.NODE_ENV == "development") {
             if (!origin) return callback(null, true);
         }
