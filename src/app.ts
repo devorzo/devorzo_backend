84340f0d642ead966c07a726f34cde5cd68aae07
Vamshi krishna <vamshi9666@riseup.net>
2020-08-10 19:18:07 +0530

    github 0auth signup  && login

--- src/app.ts
+++ src/app.ts
@@ -1,107 +1,110 @@
 // imports
-import minimist from "minimist"
-import { initEnv } from "./config/config"
-import express from "express"
+import minimist from "minimist";
+import { initEnv } from "./config/config";
+import express from "express";
 // import "ejs"
-import cors from "cors"
-import helmet from "helmet"
+import cors from "cors";
+import helmet from "helmet";
 // import path from "path"
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 
-import initDb from "./database/mongoose"
-import session from "express-session"
-import { default as connectMongoDBSession } from "connect-mongodb-session"
-import { v4 as uuid } from "uuid"
+import initDb from "./database/mongoose";
+import session from "express-session";
+import { default as connectMongoDBSession } from "connect-mongodb-session";
+import { v4 as uuid } from "uuid";
 
+let argv = minimist(process.argv.slice(2));
+initEnv(argv.env);
 
-let argv = minimist(process.argv.slice(2))
-initEnv(argv.env)
+import Services from "./services/initService";
 
+import adminApiService from "./services/micros/admin_api_service";
 
-import Services from "./services/initService"
+import articleApiService from "./services/micros/article_api_service";
+import authService from "./services/micros/auth_service";
 
-import adminApiService from "./services/micros/admin_api_service"
+import communityApiService from "./services/micros/community_api_service";
 
-import articleApiService from "./services/micros/article_api_service"
-import authService from "./services/micros/auth_service"
+import moderationApiService from "./services/micros/content_moderation_service";
+import suggestionApiService from "./services/micros/content_suggestion_service";
 
-import communityApiService from "./services/micros/community_api_service"
-
-import moderationApiService from "./services/micros/content_moderation_service"
-import suggestionApiService from "./services/micros/content_suggestion_service"
-
-import fileService from "./services/micros/file_service"
+import fileService from "./services/micros/file_service";
 
 // import notificationService from "./services/micros/notification_service"
-import uiService from "./services/micros/ui_service"
+import uiService from "./services/micros/ui_service";
 
-import userApiService from "./services/micros/user_api_service"
-import notificationApiService from "./services/micros/notification_service"
+import userApiService from "./services/micros/user_api_service";
+import notificationApiService from "./services/micros/notification_service";
 
-import logger, { Level } from "./lib/logger"
-import getServiceNames from "./lib/gather_service_names"
-import { responseMessageCreator } from "./lib/response_message_creator"
+import logger, { Level } from "./lib/logger";
+import getServiceNames from "./lib/gather_service_names";
+import { responseMessageCreator } from "./lib/response_message_creator";
 
+logger({ argv }, Level.VERBOSE);
 
-logger({ argv }, Level.VERBOSE)
+let app = express();
 
-let app = express()
-
-let whitelist = require("../whitelist.json")
+let whitelist = require("../whitelist.json");
 
 let corsOptionsDelegate = function (req: any, callback: any) {
-    let corsOptions
-    console.log(req.header("Origin"), whitelist.indexOf(req.header("Origin")))
-    if (whitelist.indexOf(req.header("Origin")) !== -1) {
-        corsOptions = { origin: true }
-    } else {
-        corsOptions = { origin: false }
-    }
-    callback(null, corsOptions)
-}
+  let corsOptions;
+  console.log(req.header("Origin"), whitelist.indexOf(req.header("Origin")));
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
 
-app.use(helmet())
-if (process.env.NODE_ENV == "development")
-    app.use(cors())
+app.use(helmet());
+if (process.env.NODE_ENV == "development") app.use(cors());
 else {
-    app.use((req, res, next) => {
-        if (whitelist.indexOf(req.header("Origin")) !== -1 &&
-            process.env.ENABLE_RFC1918) {
-            res.append("Access-Control-Allow-External", "true")
-        }
-        next()
-    })
-    app.use(cors(corsOptionsDelegate))
+  app.use((req, res, next) => {
+    if (
+      whitelist.indexOf(req.header("Origin")) !== -1 &&
+      process.env.ENABLE_RFC1918
+    ) {
+      res.append("Access-Control-Allow-External", "true");
+    }
+    next();
+  });
+  app.use(cors(corsOptionsDelegate));
 }
-
 
 /*  Database handlers */
 // eslint-disable-next-line no-unused-vars
-let mongoose = initDb()
+initDb();
 
-const MongoDBStore = connectMongoDBSession(session)
+const MongoDBStore = connectMongoDBSession(session);
 
 /* body parser */
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     limit: "50mb",
     parameterLimit: 100000,
-    extended: true
-}))
-app.use(bodyParser.json({
-    limit: "50mb"
-}))
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
 
 /* Session */
-app.use(session({
+app.use(
+  session({
     secret: uuid(),
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false },
     store: new MongoDBStore({
-        uri: process.env.MONGODB_URI!,
-        collection: "sessions"
-    })
-}))
+      uri: process.env.MONGODB_URI!,
+      collection: "sessions",
+    }),
+  })
+);
 
 /* view engine */
 // app.use(express.static(path.join(__dirname, "../") + "/build"))
@@ -110,52 +113,50 @@ app.use(session({
 // app.set("view engine", "ejs")
 
 /* Services */
-let s = new Services(app)
-s.addService("admin-api-service", adminApiService)
-s.addService("article-api-service", articleApiService)
-s.addService("auth-api-service", authService)
-s.addService("community-api-service", communityApiService)
-s.addService("moderation-api-service", moderationApiService)
-s.addService("suggestion-api-service", suggestionApiService)
-s.addService("file-service", fileService)
-s.addService("article-api-service", articleApiService)
-s.addService("ui-service", uiService)
-s.addService("user-service", userApiService)
-s.addService("notification-service", notificationApiService)
+let s = new Services(app);
+s.addService("admin-api-service", adminApiService);
+s.addService("article-api-service", articleApiService);
+s.addService("auth-api-service", authService);
+s.addService("community-api-service", communityApiService);
+s.addService("moderation-api-service", moderationApiService);
+s.addService("suggestion-api-service", suggestionApiService);
+s.addService("file-service", fileService);
+s.addService("article-api-service", articleApiService);
+s.addService("ui-service", uiService);
+s.addService("user-service", userApiService);
+s.addService("notification-service", notificationApiService);
 
-let serviceNames = getServiceNames(argv.service)
-s.serviceInit(serviceNames)
-logger({ serviceNames }, Level.VERBOSE)
+let serviceNames = getServiceNames(argv.service);
+s.serviceInit(serviceNames);
+logger({ serviceNames }, Level.VERBOSE);
 
-
-app.set("port", (process.env.PORT || 5000))
-
+app.set("port", process.env.PORT || 5000);
 
 app.get("/", (req, res) => {
-    res.send({
-        version: process.env.VERSION
-    })
-})
+  res.send({
+    version: process.env.VERSION,
+  });
+});
 
 app.get("/load", (req, res) => {
-    let mem = process.memoryUsage()
-    res.send({
-        memory: {
-            rss: mem.rss / (1024 * 1024),
-            heapTotal: mem.heapTotal / (1024 * 1024),
-            heapUsed: mem.heapUsed / (1024 * 1024),
-            external: mem.external / (1024 * 1024)
-        }
-    })
-})
+  let mem = process.memoryUsage();
+  res.send({
+    memory: {
+      rss: mem.rss / (1024 * 1024),
+      heapTotal: mem.heapTotal / (1024 * 1024),
+      heapUsed: mem.heapUsed / (1024 * 1024),
+      external: mem.external / (1024 * 1024),
+    },
+  });
+});
 
 // res.sendFile(path.join(__dirname + '/client/public/index.html'));
 
 app.get("*", (req, res) => {
-    res.status(404).send(responseMessageCreator("Bad Request", 0))
-})
+  res.status(404).send(responseMessageCreator("Bad Request", 0));
+});
 
 app.listen(app.get("port"), process.env.IP!, function () {
-    logger(`Server started at ${app.get("port")}`, Level.INFO)
-    logger(`env: ${process.env.NODE_ENV}`)
-})
+  logger(`Server started at ${app.get("port")}`, Level.INFO);
+  logger(`env: ${process.env.NODE_ENV}`);
+});
