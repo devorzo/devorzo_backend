5ca268398cc7370eb8c66bfb2e2b2c3efe9df7d5
Vamshi krishna <vamshi9666@riseup.net>
2020-08-22 16:11:45 +0530

    fix: eslint config root

--- src/lib/parseToken.ts
+++ src/lib/parseToken.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { decode } from 'jsonwebtoken';
 
 const parseToken = (token:string): string => {
   const payload = decode(token);
   // @ts-ignore
-  return payload.id;
+  return payload ? payload.id : '';
 };
 export default parseToken;
